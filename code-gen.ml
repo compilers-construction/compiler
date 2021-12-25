8fd0da37d5e1a9b6d0d3920f61af723eb1d1f6b4
LZL <liorz@rezilion.com>
2021-12-25 02:14:58 +0200

    Final project skeleton

---
+++ code-gen.ml
@@ -0,0 +1,39 @@
+#use "semantic-analyser.ml";;
+
+(* This module is here for you convenience only!
+   You are not required to use it.
+   you are allowed to change it. *)
+module type CODE_GEN = sig
+  (* This signature assumes the structure of the constants table is
+     a list of key-value pairs:
+     - The keys are constant values (Sexpr(x) or Void)
+     - The values are pairs of:
+       * the offset from the base const_table address in bytes; and
+       * a string containing the byte representation (or a sequence of nasm macros)
+         of the constant value
+     For example: [(Sexpr(Nil), (1, "T_NIL"))]
+   *)
+  val make_consts_tbl : expr' list -> (sexpr * (int * string)) list
+
+  (* This signature assumes the structure of the fvars table is
+     a list of key-value pairs:
+     - The keys are the fvar names as strings
+     - The values are the offsets from the base fvars_table address in bytes
+     For example: [("boolean?", 0)]
+   *)  
+  val make_fvars_tbl : expr' list -> (string * int) list
+
+  (* If you change the types of the constants and fvars tables, you will have to update
+     this signature to match: The first argument is the constants table type, the second 
+     argument is the fvars table type, and the third is an expr' that has been annotated 
+     by the semantic analyser.
+   *)
+  val generate : (sexpr * (int * string)) list -> (string * int) list -> expr' -> string
+end;;
+
+module Code_Gen : CODE_GEN = struct
+  let make_consts_tbl asts = raise X_not_yet_implemented;;
+  let make_fvars_tbl asts = raise X_not_yet_implemented;;
+  let generate consts fvars e = raise X_not_yet_implemented;;
+end;;
+
